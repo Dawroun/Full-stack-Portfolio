74a61b0bef9697c2242bcb646870c397094a6c53
Davron Abdukhakimov <davron2806@gmail.com>
2022-08-17 17:38:00 +0500

    connected backend with frontend and added about section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import './About.scss';
+import React, { useEffect, useState } from "react";
+import "./About.scss";
+import { motion } from "framer-motion";
+
+import { images } from "../../constants";
+import { urlFor, client } from "../../client";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
 
-export default About
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, []);
+  
+
+  return (
+    <>
+      <h2 className="head-text">
+        I know that <span>Good Development</span> <br /> means{" "}
+        <span>Good Bussiness</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
